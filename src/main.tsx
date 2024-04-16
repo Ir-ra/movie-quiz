c8a7ba2761bcd97025af6cb9543450d2e08e3459
Ir-ra <chapter.ku@ukr.net>
2024-04-16 12:07:46 +0300

    fix: BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
-import { BrowserRouter as Router, } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 const BASE_URL = '/movie-quiz/';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router basename={BASE_URL}>
+    <BrowserRouter basename={BASE_URL}>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>,
 )
